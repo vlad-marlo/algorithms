9564ad0becee0ea63f03a39e8d5eac894c67e212
Vlad Marlo <vlad.marlo@ya.ru>
2023-03-04 23:43:08 +0300

    added inline solutions of 27 numbers

--- school/webium/hw/27/first/solution.py
+++ school/webium/hw/27/first/solution.py
@@ -4,10 +4,23 @@ def read_data(file: str) -> list[int]:
 
 
 def solution(data: list[int]) -> int:
+    indexes = [-1]
+    for index in range(len(data)):
+        n = data[index]
+        if int(n ** 0.5) ** 2 == n and n != 1:
+            indexes.append(index)
+    m = 0
+    for index in range(len(indexes) - 1):
+        start, end = indexes[index], indexes[index + 1]
+        m = max(m, end - start - 1)
+    return m
+
+
+def solution_one_line(data: list[int]) -> int:
     idx = [-1, *(i for i in range(len(data)) if int((n := data[i]) ** 0.5) ** 2 == n and n != 1), len(data)]
     return max([idx[i + 1] - idx[i] - 1 for i in range(len(idx) - 1)])
 
 
-print(solution(read_data('27_a.txt')))
-print(solution([1, 4, 14, 5, 9, 1, 2, 3, 8, 9]))
-print(solution(read_data('27_b.txt')))
+print(solution(read_data('27_a.txt')), solution_one_line(read_data('27_a.txt')))
+print(solution([1, 4, 14, 5, 9, 1, 2, 3, 8, 9]), solution_one_line([1, 4, 14, 5, 9, 1, 2, 3, 8, 9]))
+print(solution(read_data('27_b.txt')), solution_one_line(read_data('27_b.txt')))
--- school/webium/hw/27/second/solution.py
+++ school/webium/hw/27/second/solution.py
@@ -13,6 +13,10 @@ def solution(data: list[int]) -> int:
     return c
 
 
-print(solution([2, 5, 3, 14, 27]))
-print(solution(read_data('27_a.txt')))
-print(solution(read_data('27_b.txt')))
+def solution_one_line(data: list[int]) -> int:
+    return len([1 for j in range(1, len(data), 3) if data[(j - 1) // 3] * 2.5 == data[j] or data[j] * 2.5 == data[(j - 1) // 3]])
+
+
+print(solution([2, 5, 3, 14, 27]), solution_one_line([2, 5, 14, 27]))
+print(solution(read_data('27_a.txt')), solution_one_line(read_data('27_a.txt')))
+print(solution(read_data('27_b.txt')), solution_one_line(read_data('27_b.txt')))
